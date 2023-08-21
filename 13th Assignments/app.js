0ecc386f8e7902be2d3f3e54092d706440b8083c
talhashaz <135649557+talhashaz@users.noreply.github.com>
2023-08-21 21:43:34 +0500

    Add files via upload

---
+++ 13th Assignments/app.js
@@ -0,0 +1,14 @@
+alert("Please Enter a valid password")
+alert("My Name is Talha")
+alert("Hello World")
+
+
+
+
+
+var lanakiyahai = "dahi";
+
+if(lana kiya hai === "dahi"){
+    console.log("tw lai ana")
+}
+
